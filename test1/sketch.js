de369a8c1aacacbbecc0f5693bf615b98b4cdaa7
younghong <durtiful82@gmail.com>
2021-02-19 18:58:24 +0900

    2

--- test1/sketch.js
+++ test1/sketch.js
@@ -1,8 +1,36 @@
+var targety;
+var ty;
+
 function setup(){
-    createCanvas(400,400);
+    let myCanvas = createCanvas(600,400);
+
+    myCanvas.parent('myContainer');
+
+    drawingContext.shadowOffsetX= 5;
+    drawingContext.shadowOffsetY= -5;
+    drawingContext.shadowBlur = 10;
+    drawingContext.shadowColor = 'Black';
+    background(200);
+    //ellipse(width/2,height/2,50,50);
+
+    targety = 0;
+    ty=1;
 }
 
 function draw(){
+    background(200);
+    ellipse(width/2,targety,50,50);
+
+    if( 400 < targety ){
+        ty=-1;
+    }else if( 0> targety ){
+        ty=1;
+    }
+
+    targety+=(10*ty);
+}
+
+function draw2(){
     if( mouseIsPressed ){
         fill(0);
     }else{
